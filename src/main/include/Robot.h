d56c746af98e148882827f86d7c1c0a101366926
Trent Hekman <trenthekman68@gmail.com>
2024-02-09 19:36:17 -0600

    add wait at position

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -10,19 +10,22 @@
 
 class Robot : public frc::TimedRobot {
  public:
-  int x = 0;
+  int x = 0; // current autoPos setpoint index
+  int i = 0; // next position index while waiting
   struct autoValue{
     complex<double> pos;
-    double angle;
+    double angle = 0;
+    units::time::second_t holdTime = 0_s;
   };
   frc::XboxController controller{0};
   Swerve swerve;
   autoValue autoPos[4] = {
-    {complex<double>(15, 0), 0},
-    {complex<double>(15, 15), 0},
-    {complex<double>(0, 15), 0},
-    {complex<double>(0, 0), 0}
+    {complex<double>(15, 0), 0, 1_s},
+    {complex<double>(15, 15), 0, 1_s},
+    {complex<double>(0, 15), 0, 1_s},
+    {complex<double>(0, 0), 0, 1_s}
   };
+  frc::Timer posHold;
   void RobotInit() override;
   void RobotPeriodic() override;
 
