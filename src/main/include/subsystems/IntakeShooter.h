a5a7850244b7eff6fa800434ea93c0fb8d11e93a
Trent Hekman <122338142+Trent008@users.noreply.github.com>
2024-02-15 18:01:26 -0600

    update

--- src/main/include/subsystems/IntakeShooter.h
+++ src/main/include/subsystems/IntakeShooter.h
@@ -57,7 +57,7 @@ public:
 
 		m_angle.RestoreFactoryDefaults();
 		m_angle.SetInverted(false);
-		anglePID.SetP(0.2);
+		anglePID.SetP(0.1);
 		anglePID.SetI(0);
 		anglePID.SetD(1);
 		anglePID.SetFF(0);
